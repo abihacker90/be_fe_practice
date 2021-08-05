ec2646bb892579bfa6a104da8bf3ffb642e2a424
Adam Bihacker <adam.bihacker@gmail.com>
2021-08-05 09:08:45 +0200

    form

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,32 @@
 const express = require('express');
 const path = require('path');
-
 const server = express();
+
 server.use('/pub', express.static(path.join(__dirname, '../frontend/pub/')));
 
 server.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/index.html')
-       
+    
     );
 });
+server.use(function (req, res, next) {
+    req.headers['content-type'] = 'application/json';
+    next();
+  });
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+server.use(function (req, res, next) {
+    //req.headers['content-type'] = 'application/json';
+    next();
+  });
 
 server.post('/', (req, res) => {
-    
-    res.send("Welcome")
-  });
-  
+    req.header('Content-Type', 'application/json');
+    console.log("body:", req.body.firstName);
+});
 
- 
-const port = 6789;
+const port = 7890;
 server.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
--- frontend/pub/script.js
+++ frontend/pub/script.js
@@ -2,20 +2,42 @@ function _load() {
      const rootElement = document.getElementById('root');
 
      rootElement.insertAdjacentHTML('beforeend', `
-        <button id="send">Go Post!</button>
-     `)
+        <form>
+            <input class="input" name="firstName" placeholder="first name"/>
+            <input class="input" name="lastName" placeholder="last name"/>
+            <input class="input" name="email" type="email" placeholder="email"/>
+            <textarea class="input" name="message" placeholder="message"></textarea>
+            <button type="submit" id="send">Send</button>
+        </form>
+     
+     `);
     function postToClick(e) {
+        e.preventDefault();
+
+        const inputs = e.target.querySelectorAll('.input'), values = {};
+
+        for (const input of inputs) {
+            
+            values[`${input.name}`] = input.value;
+
+        }
+        console.log(values);
+
+        /* const fd = new FormData();
+        fd.append('json', JSON.stringify(values)); */
+
+
         fetch('/', {
             method: 'Post',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: "HELLO"
+            body: JSON.stringify(values)
         })
         .then(response => response.text())
         .then(data => console.log(data));
     }
-     document.getElementById("send").addEventListener('click', postToClick);
+     window.addEventListener('submit', postToClick);
      console.log('Frontend betöltődött');
 
 }
